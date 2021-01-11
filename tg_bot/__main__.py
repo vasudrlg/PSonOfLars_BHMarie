084a347fffda05c6340b69c6df0c2fab5be20ae0
vasudrlg <75256473+vasudrlg@users.noreply.github.com>
2021-01-11 09:05:30 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,17 +20,10 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
 
 *Main* commands available:
  - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+ 
 
 {}
 And the following:
